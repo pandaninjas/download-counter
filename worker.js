4f5dc2fe0d10483c0a349d66de0738e11fb2c654
pandaninjas <101084582+pandaninjas@users.noreply.github.com>
2022-11-09 14:04:06 -0800

    Initial commit

---
+++ worker.js
@@ -0,0 +1,29 @@
+export default {
+  async fetch(request, env) {
+    return await handleRequest(request)
+  }
+}
+
+async function handleRequest(request) {
+  let url = new URL(request.url);
+  let repo = url.pathname.split("/")[1] + "/" + url.pathname.split("/")[2];
+  let apiresp = await fetch(
+    `https://api.github.com/repos/${repo}/releases`,
+    {
+      headers: {
+        "User-Agent": "DownloadCounter (+github.com/pandaninjas/download-counter)"
+      }
+    }
+  ).then(res => res.json());
+  let sum = 0;
+  for (let i = 0; i < apiresp.length; i++) {
+    let release = apiresp[0];
+    for (let j = 0; j < release.assets.length; j++) {
+      let asset = release.assets[j];
+      if (url.searchParams.get("filter") == null || asset.name.endsWith(url.searchParams.get("filter"))) {
+        sum += asset.download_count
+      }
+    }
+  }
+  return await fetch(`https://img.shields.io/badge/downloads-${sum}-green`);
+}
